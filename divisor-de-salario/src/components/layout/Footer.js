f904a477192de7a43fb331aa6e12909d17c76f60
Rafael Menezes <faelgmp@gmail.com>
2023-06-02 18:45:27 -0300

    footer completo

--- divisor-de-salario/src/components/layout/Footer.js
+++ divisor-de-salario/src/components/layout/Footer.js
@@ -1,7 +1,25 @@
+import {BsGithub, BsLinkedin, BsHddStackFill} from "react-icons/bs";
+
+import styles from "./Footer.module.css";
+
 function Footer(){
     return (
-        <footer>
-
+        <footer className={styles.footer}>
+            <nav>
+                <a rel='noreferrer external' target='_blank' href="https://github.com/RGMenezes">
+                    <BsGithub className={styles.icon} />
+                    <p>@RGMenezes</p>
+                </a>
+                <a rel='noreferrer external' target='_blank' href="https://www.linkedin.com/in/rafael-menezes-16b002246/">
+                    <BsLinkedin className={styles.icon} />
+                    <p>@RafaelMenezes</p>
+                </a>
+                <a rel='noreferrer external' target='_blank' href="https://github.com/RGMenezes/divisor-de-salario">
+                    <BsHddStackFill className={styles.icon} />
+                    <p>Repositório</p>
+                </a>
+            </nav>
+            <p>Divisor de salário &copy; 2023</p>
         </footer>
     );
 };
