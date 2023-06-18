9e9356868f442ca31397f353c2e532e784dcd7a2
Rafael Menezes <faelgmp@gmail.com>
2023-06-18 18:10:07 -0300

    corrigindo URL

--- divisor-de-salario/src/api/db.js
+++ divisor-de-salario/src/api/db.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     //opsteriormente está url será substituida pela url final.
-    baseURL: "https://db-divisor-de-salario.vercel.app/"
+    baseURL: "https://db-divisor-de-salario-mlpescw1q-rgmenezes.vercel.app/"
 });
